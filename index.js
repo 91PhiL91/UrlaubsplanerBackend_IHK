591f8772400733e4aa0a01b6f752743da8d91ab8
PhilipP <--->
2023-03-20 09:27:30 +0100

    Struktur erstellt.

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'); // Importieren des Express-Frameworks
+const app = express(); // Erstellen einer Express-App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => { // Starten des Servers auf Port 3000
+    console.log('Server started on port 3000'); // Ausgabe in der Konsole
+  });
