b8e12e196b942d30e4d42588e9a51c1134ed94d6
PhilipP <--->
2023-03-23 10:32:29 +0100

    CORSE Police I.O

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const service = require('./controller/serviceController')
 const Sequelize = require('sequelize');
+const cors = require('cors');
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,8 +26,8 @@ app.use('/', service);
 
 
 
-module.exports = router; 
+module.exports = router;
 
-app.listen(3005, () => { // Starten des Servers auf Port 3000
-    console.log('Server started on port 3000'); // Ausgabe in der Konsole
-  });
+app.listen(3000, () => { // Starten des Servers auf Port 3000
+  console.log('Server started on port 3000'); // Ausgabe in der Konsole
+});
